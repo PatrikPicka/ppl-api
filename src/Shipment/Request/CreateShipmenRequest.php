ea84285bcf03262b3de24b5afcb68b783fe95432
PatrikPicka <patrik@Patrik>
2025-01-15 22:15:49 +0100

    finalization

---
+++ src/Shipment/Request/CreateShipmenRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PTB\PPLApi\Shipment\Request;
+
+use JsonSerializable;
+use PTB\PPLApi\Shipment\Request\Data\ShipmentData;
+use PTB\PPLApi\Shipment\Request\Setting\LabelSetting;
+
+class CreateShipmenRequest implements JsonSerializable
+{
+	public function __construct(
+		public LabelSetting $labelSetting,
+		public ShipmentData $shipmentData,
+	) {
+	}
+
+	public function jsonSerialize(): array
+	{
+		return [
+			'labelSettings' => $this->labelSetting->jsonSerialize(),
+			'shipments' => [
+				$this->shipmentData->jsonSerialize()
+			],
+		];
+	}
+}
